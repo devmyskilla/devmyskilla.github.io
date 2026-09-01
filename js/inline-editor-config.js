210ff4a2d041b89ab879a4e297dc3350e22dbfa6
Aasim Mohammed Altomi <atomy8774@gmail.com>
2026-09-01 03:55:30 +0300

    feat: configure public inline editor endpoints

---
+++ js/inline-editor-config.js
@@ -0,0 +1,8 @@
+(function(root){
+  root.InlineEditorConfig=Object.freeze({
+    apiBase:'https://dunya-inline-editor.atomy8774.workers.dev',
+    siteOrigin:'https://devmyskilla.github.io',
+    adminUrl:'admin/',
+    sessionKey:'dunya-inline-session'
+  });
+})(typeof globalThis!=='undefined'?globalThis:this);
